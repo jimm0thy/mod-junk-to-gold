5ee70a200fc12f4898f1f25933c6222bd27740b3
jimm0thy <25890053+jimm0thy@users.noreply.github.com>
2025-02-24 12:11:54 -0500

    Dont Sell Player Loot Toggle
    Added option to enable / disable auto selling of real players gray loot. To be used in conjunction with playerbots

--- src/mod_junk_to_gold.cpp
+++ src/mod_junk_to_gold.cpp
@@ -1,11 +1,40 @@
 #include "Chat.h"
 #include "Player.h"
 #include "ScriptMgr.h"
+#include "DatabaseEnv.h"
+#include "QueryResult.h"
+#include "Config.h"
+#include "WorldSession.h"
 
-class JunkToGold : public PlayerScript
+
+// Variables to detrmine if we auto sell for real players and bots
+static bool IgnoreRealPlayers = true;
+static std::string BotAccountPrefix = "rndbot";
+
+// Loads Config Values
+static void LoadJunkToGoldConfig()
+{
+    IgnoreRealPlayers = sConfigMgr->GetOption<bool>("JunkToGold.IgnoreRealPlayers", true);
+    BotAccountPrefix = sConfigMgr->GetOption<std::string>("JunkToGold.BotAccountPrefix", "rndbot");
+}
+
+class JunkToGoldWorldScript : public WorldScript
 {
 public:
-    JunkToGold() : PlayerScript("JunkToGold") {}
+    JunkToGoldWorldScript() : WorldScript("JunkToGoldWorldScript") {}
+
+    void OnStartup() override
+    {
+        LOG_INFO("server.loading", "[Junk2Gold] Loading Configuration");
+        LoadJunkToGoldConfig();
+    }
+};
+
+class JunkToGold : public PlayerScript
+{   
+
+public:
+    JunkToGold() : PlayerScript("JunkToGold") {}       
 
     void OnPlayerLootItem(Player* player, Item* item, uint32 count, ObjectGuid /*lootguid*/) override
     {
@@ -13,6 +42,22 @@ public:
         {
             return;
         }
+
+        // Make this apply only to bots
+        if (IgnoreRealPlayers)
+        {
+            QueryResult result = LoginDatabase.Query("SELECT COUNT(username) FROM account WHERE id IN (SELECT account FROM acore_characters.characters WHERE NAME = '{}') AND username NOT LIKE '%{}%'", player->GetName(), BotAccountPrefix);
+            uint32 resultCount = 0;
+            resultCount = result->Fetch()->Get<uint32>();
+
+            if (resultCount >= 1)
+            {
+                // This is a real player, dont sell their junk
+                LOG_INFO("module", "[Junk2Gold] Player {} is a real person, not selling their junk", player->GetName());
+                return;
+            }
+        }
+
 
         if (item->GetTemplate()->Quality == ITEM_QUALITY_POOR)
         {
